7e9a2f4d5f3b7ef45a1712d464960c502450ec8d
argyaWicaksana <argya7@gmail.com>
2023-02-23 23:18:37 +0700

    add update feature

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { useEffect, useState } from "react"
-import { PencilSquareIcon, TrashIcon } from '@heroicons/react/24/outline'
+import { FormEvent, useEffect, useState } from "react"
+import { PencilSquareIcon, TrashIcon, XMarkIcon } from '@heroicons/react/24/outline'
 
 interface Order {
   _id: { $oid: string },
@@ -9,7 +9,68 @@ interface Order {
   size: number
 }
 
-function RowData({ d, onUpdate }: { d: Order, onUpdate: ()=> void }) {
+interface TableProps {
+  data: Order[] | undefined,
+  onUpdate: () => void,
+}
+
+function FormModal({ closeModal, data, onUpdate }: { closeModal: () => void, data: Order, onUpdate: () => void }) {
+  const [dataInput, setDataInput] = useState<Order>(data)
+
+  const updateOrder = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    closeModal()
+
+    const name = (e.currentTarget.elements.namedItem('name') as HTMLInputElement).value
+    const address = (e.currentTarget.elements.namedItem('address') as HTMLInputElement).value
+    const size = (e.currentTarget.elements.namedItem('size') as HTMLInputElement).value
+
+    const response = await fetch('http://localhost:5000/mars/update', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        id: data._id.$oid,
+        name: name,
+        address: address,
+        size: size
+      })
+    })
+    const msg = await response.json()
+    console.log(msg)
+
+    onUpdate()
+  }
+
+  return (
+    <div className="fixed inset-0 grid place-items-center bg-[rgba(0,0,0,0.5)]">
+      <div className="border border-slate-700 bg-slate-900 sm:w-3/6 xl:w-2/6">
+        <div className="p-3 border-b border-slate-700 flex justify-between">
+          <h1>Update Form</h1>
+          <XMarkIcon className="h-6 w-6 cursor-pointer" onClick={closeModal} />
+        </div>
+        <form className="p-16 flex flex-col gap-4" onSubmit={updateOrder}>
+          <input type="text" onChange={(e) => setDataInput({ ...dataInput, name: e.target.value })}
+            name="name" placeholder="Name" className="p-2 text-black" value={dataInput.name} required />
+          <input type="text" onChange={(e) => setDataInput({ ...dataInput, address: e.target.value })}
+            name="address" placeholder="Address" className="p-2 text-black" value={dataInput.address} required />
+          <select name="size" onChange={(e) => setDataInput({ ...dataInput, size: Number(e.target.value) })}
+          className="border p-2 text-slate-700 bg-white" placeholder="Select acreage" value={dataInput.size}>
+            <option value="10">10 acres</option>
+            <option value="20">20 acres</option>
+            <option value="30">30 acres</option>
+            <option value="40">40 acres</option>
+            <option value="50">50 acres</option>
+          </select>
+          <button type="submit" className="btn">Update</button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+function RowData({ d, onUpdate }: { d: Order, onUpdate: () => void }) {
+  const [showModal, setShowModal] = useState<boolean>(false)
+
   const deleteOrder = async () => {
     const response = await fetch('http://localhost:5000/mars/delete', {
       method: 'POST',
@@ -24,24 +85,22 @@ function RowData({ d, onUpdate }: { d: Order, onUpdate: ()=> void }) {
     onUpdate()
   }
 
-  const updateOrder = async () => {
-        
-  }
-
   return (
     <tr className="border-b" key={d._id.$oid}>
       <td>{d.name}</td>
       <td>{d.address}</td>
       <td>{d.size}</td>
-      <td className="space-x-4">
-        <button><PencilSquareIcon className="h-5 w-5" /></button>
-        <button onClick={deleteOrder}><TrashIcon className="h-5 w-5" /></button>
+      <td className="flex gap-4">
+        <PencilSquareIcon onClick={() => setShowModal(true)} className="h-5 w-5 cursor-pointer" />
+        <TrashIcon onClick={deleteOrder} className="h-5 w-5 cursor-pointer" />
+        {showModal && <FormModal closeModal={() => setShowModal(false)} data={d} onUpdate={onUpdate} />}
+
       </td>
     </tr>
   )
 }
 
-function Table({ data, onUpdate }: { data: Order[] | undefined, onUpdate: ()=> void }) {
+function Table({ data, onUpdate }: TableProps) {
 
   return (
     <table className="table-auto border-collapse border-white w-full text-left">
@@ -56,7 +115,7 @@ function Table({ data, onUpdate }: { data: Order[] | undefined, onUpdate: ()=> v
       <tbody>
         {data ?
           data.map((d) => (
-            <RowData d={d} onUpdate={onUpdate} />
+            <RowData key={d._id.$oid} d={d} onUpdate={onUpdate} />
           ))
           :
           <tr>
@@ -118,7 +177,7 @@ export default function Home() {
             <option value="40">40 acres</option>
             <option value="50">50 acres</option>
           </select>
-          <button type="submit" className="p-2 bg-yellow-400 text-black">Create order</button>
+          <button type="submit" className="btn">Create order</button>
         </form>
         <Table data={data} onUpdate={getData} />
       </div>
